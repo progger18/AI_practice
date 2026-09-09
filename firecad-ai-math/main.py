15c19b0374c9ce73f949bfa64b7de3e0e04d985b
Amir Zakirov <knyazamir@gmail.com>
2026-09-09 11:41:08 +0300

    Day 1

---
+++ firecad-ai-math/main.py
@@ -0,0 +1,33 @@
+w = 2
+b = 1
+
+print('x | prediction')
+print('---------------')
+
+x = 1
+y = w * x + b
+print('1 | ', y)
+
+x = 2
+y = w * x + b
+print('2 | ', y)
+
+x = 3
+y = w * x + b
+print('3 | ', y)
+
+x = 4
+y = w * x + b
+print('4 | ', y)
+
+x = 5
+y = w * x + b
+print('5 | ', y)
+
+# x | prediction
+# --------------
+# 1 | ?
+# 2 | ?
+# 3 | ?
+# 4 | ?
+# 5 | ?
